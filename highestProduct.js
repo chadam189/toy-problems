5f96e39d2a1bb857c0698ad0b2395d421f8c9b93
Adam Miller <chadam189@gmail.com>
2018-01-30 16:43:45 -0800

    Skeleton for highestProduct

---
+++ highestProduct.js
@@ -0,0 +1,21 @@
+const nums = [1, 10, -5, 1, -100];
+
+/* 
+
+SETUP
+
+Strategy
+
+Track:
+
+Largest three positives, largest two negatives
+
+input = array
+output = product
+
+const highestProductOfThree = (nums) => {
+	
+};
+
+
+*/
