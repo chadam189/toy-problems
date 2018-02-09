6977593c71c0d70198665f0eb1e4e6ee509f4bac
Adam Miller <chadam189@gmail.com>
2018-02-08 22:49:04 -0800

    Complete bubbleSort from CTCI

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,3 +1,33 @@
+const bubbleSort = (nums) => {
+  for (let n = nums.length - 1; n > 0; n--) {
+    let swapped = false;
+    let i = 0;
+    while (i < n) {
+      if (nums[i] > nums[i + 1]) {
+        let temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+      }
+      i++;
+    }
+    if (!swapped) {
+      return nums;
+    }
+  }
+  return nums;
+};
+
+let input = [5, 1, 8, 4, 2, 6];
+let result = [1, 2, 4, 5, 6, 8];
+
+console.log(`it should return a sorted array: ${JSON.stringify(bubbleSort(input)) === JSON.stringify(result)}`);
+
+input = [8, 6, 5, 4, 2, 1];
+result = [1, 2, 4, 5, 6, 8];
+
+console.log(`it should sort a reversed array: ${JSON.stringify(bubbleSort(input)) === JSON.stringify(result)}`);
+
 /* 
 
 SETUP
@@ -19,15 +49,15 @@ round 3: [1, 2, 4, 5, 6, 8] => no swaps made, so we return
 Output: const result = [1, 2, 4, 5, 6, 8];
 
 const bubbleSort = (nums) => {
-	let n = nums.length - 1;
-	// for each num...
-	  let swapped = false;
-	  let j = 0;
-	  // while j < n
-		  // if j > j + 1, swap
-		    // swap numbers
-		    // swapped = true;
-		  // increment j
+  let n = nums.length - 1;
+  // for each num...
+    let swapped = false;
+    let j = 0;
+    // while j < n
+      // if j > j + 1, swap
+        // swap numbers
+        // swapped = true;
+      // increment j
     // if swapped is false, return
 };
 
