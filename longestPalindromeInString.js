5a5e92a6b0816c5d90963b95ad39fc305c8109fc
Adam Miller <chadam189@gmail.com>
2018-02-12 20:46:53 -0800

    Complete longestPalindromeInString from LeetCode

--- longestPalindromeInString.js
+++ longestPalindromeInString.js
@@ -22,33 +22,55 @@ Output: "bb"
 */
 
 
-const findLongestPalindrome = (str) => {
-	let currP = [];
-	let maxP = [];
+const longestPalindrome = (str) => {
 
-	// for each char...
-	  // search for middle point
-	    // if current char equals previous char, we've found a midpoint
-	    // else, if current char equals previous previous char, the previous char is a midpoint
-	    // if midpoint is found, enter while loop to find length of pally
-	      // current pally = midpoint
-	      // use two pointers - both at midpoint
-	      // while left pointer >= 0 and right pointer < string.length
-	        // if char at both pointers is same
-	          // add that char to both front and end of current palindrome
-          // else, stop the while loop
-	  // if current palindrome is longer than max palindrome
-	    // update max palindrome
+  if (str.length < 2) {
+    return str;
+  }
 
-	return maxP;
+  let currPalindrome = '';
+  let maxPalindrome = '';
+  
+  for (let i = 1; i < str.length; i++) {
+
+    let pally1 = expandFromCenter(str, i, i);
+    let pally2 = expandFromCenter(str, i - 1, i);
+
+    currPalindrome = pally1.length > pally2.length ? pally1 : pally2;
+
+    maxPalindrome = currPalindrome.length > maxPalindrome.length ? currPalindrome : maxPalindrome;
+  }
+
+  return maxPalindrome;
+};
+
+const expandFromCenter = (str, left, right) => {
+  let palindrome = '';
+
+  if (left === right) {
+    palindrome = str.charAt(left--);
+    right++;
+  } 
+  
+
+  while (left >= 0 && right < str.length && str.charAt(left) === str.charAt(right)) {
+      palindrome = str.charAt(left--) + palindrome + str.charAt(right++);
+  }
+
+  return palindrome;
 };
 
 
 let input = 'csenutacocatdfb';
 let expected = 'tacocat';
 
-console.log(`Input of ${JSON.stringify(input)}should find the longest palindrome substring, which should be ${expected} and comes out as: ${findLongestPalindrome(input)}`);
+console.log(`Input of ${input} should find the longest palindrome substring, which should be ${expected} and comes out as: ${longestPalindrome(input)}`);
 
+
+input = 'cbbd';
+expected = 'bb';
+
+console.log(`Input of ${input} should find the longest palindrome substring, which should be ${expected} and comes out as: ${longestPalindrome(input)}`);
 
 /*
 
@@ -62,25 +84,25 @@ Input: let input = 'csenutacocatdfb';
 
 Output: let expected = 'tacocat';
 
-const findLongestPalindrome = (str) => {
-	let currP = [];
-	let maxP = [];
+const longestPalindrome = (str) => {
+  let currP = [];
+  let maxP = [];
 
-	// for each char...
-	  // search for middle point
-	    // if current char equals previous char, we've found a midpoint
-	    // else, if current char equals previous previous char, the previous char is a midpoint
-	    // if midpoint is found, enter while loop to find length of pally
-	      // current pally = midpoint
-	      // use two pointers - both at midpoint
-	      // while left pointer >= 0 and right pointer < string.length
-	        // if char at both pointers is same
-	          // add that char to both front and end of current palindrome
+  // for each char...
+    // search for middle point
+      // if current char equals previous char, we've found a midpoint
+      // else, if current char equals previous previous char, the previous char is a midpoint
+      // if midpoint is found, enter while loop to find length of pally
+        // current pally = midpoint
+        // use two pointers - both at midpoint
+        // while left pointer >= 0 and right pointer < string.length
+          // if char at both pointers is same
+            // add that char to both front and end of current palindrome
           // else, stop the while loop
-	  // if current palindrome is longer than max palindrome
-	    // update max palindrome
+    // if current palindrome is longer than max palindrome
+      // update max palindrome
 
-	return maxP;
+  return maxP;
 };
 
 
