48bd9d08cca91f02e245defa1fa51f6c272ac33b
Adam Miller <chadam189@gmail.com>
2018-02-13 20:47:42 -0800

    Complete iterative and recursive solutions for binarySearch

--- binarySearch.js
+++ binarySearch.js
@@ -15,26 +15,32 @@ const binarySearchIterative = (nums, target) => {
     }
   }
 
+  // catches errors
   return false;
 };
 
-const binarySearchRecursive = (nums, target) => {
-  let left = 0;
-  let right = nums.length - 1;
-  let middle;
+const binarySearchRecursive = (nums, target, left = 0, right = nums.length -1) => {
+  searchCount++;
 
-  while (left <= right) {
-    searchCount++;
-    middle = Math.floor((left + right) / 2);
-    if (nums[middle] === target) {
-      return true;
-    } else if (target > nums[middle]) {
-      left = middle + 1;
-    } else if (target < nums[middle]) {
-      right = middle - 1;
-    }
+  if (left > right) {
+    return false;
   }
 
+  let middle = Math.floor((left + right) / 2);
+
+  if (nums[middle] === target) {
+    return true;
+  }
+
+  else if (target > nums[middle]) {
+    return binarySearchRecursive(nums, target, middle + 1, right);
+  }
+
+   else if (target < nums[middle]) {
+    return binarySearchRecursive(nums, target, left, middle - 1);
+  }
+  
+  // catches errors
   return false;
 };
 
@@ -55,10 +61,19 @@ expectedSearchCount = 3
 console.log(`Input of ${nums} and target of ${target} should return ${expected} with a total comparison count of ${expectedSearchCount}: ${binarySearchIterative(nums, target)} with ${searchCount} search(es)`);
 
 nums = [1, 2, 4, 5, 7, 9, 12, 14, 16]
+target = 16;
+expected = true;
+searchCount = 0;
+expectedSearchCount = 4
+
+console.log(`Input of ${nums} and target of ${target} should return ${expected} with a total comparison count of ${expectedSearchCount}: ${binarySearchIterative(nums, target)} with ${searchCount} search(es)`);
+
+nums = [1, 2, 4, 5, 7, 9, 12, 14, 16]
 target = 0;
 expected = false;
 searchCount = 0;
-expectedSearchCount = 4
+// expectedSearchCount = 3 // iterative
+expectedSearchCount = 4 // recursive
 
 console.log(`Input of ${nums} and target of ${target} should return ${expected} with a total comparison count of ${expectedSearchCount}: ${binarySearchIterative(nums, target)} with ${searchCount} search(es)`);
 
@@ -66,7 +81,8 @@ nums = [1, 2, 4, 5, 7, 9, 12, 14, 16]
 target = 17;
 expected = false;
 searchCount = 0;
-expectedSearchCount = 4
+// expectedSearchCount = 4 // iterative
+expectedSearchCount = 5 // recursive
 
 console.log(`Input of ${nums} and target of ${target} should return ${expected} with a total comparison count of ${expectedSearchCount}: ${binarySearchIterative(nums, target)} with ${searchCount} search(es)`);
 
@@ -74,7 +90,8 @@ nums = [1, 2, 4, 5, 7, 9, 12, 14, 16]
 target = 3;
 expected = false;
 searchCount = 0;
-expectedSearchCount = 4
+// expectedSearchCount = 3 // iterative
+expectedSearchCount = 4 // recursive
 
 console.log(`Input of ${nums} and target of ${target} should return ${expected} with a total comparison count of ${expectedSearchCount}: ${binarySearchIterative(nums, target)} with ${searchCount} search(es)`);
 
@@ -88,19 +105,19 @@ SETUP
 
 Iterative Strategy: 
 
-	Track three pointers which will correspond to indices in the input array: left (init to 0), middle, and right (init to array length - 1, aka last index in array). 
+  Track three pointers which will correspond to indices in the input array: left (init to 0), middle, and right (init to array length - 1, aka last index in array). 
 
-	While left pointer is less than/equal to the right pointer, perform the following loop:
+  While left pointer is less than/equal to the right pointer, perform the following loop:
 
-		1) Recalculate middle pointer to be floor of (left + right) / 2.
+    1) Recalculate middle pointer to be floor of (left + right) / 2.
 
-		2) If middle element (the element in the arra at middle pointer) equals target, return true.
+    2) If middle element (the element in the arra at middle pointer) equals target, return true.
 
-		3) However, if target is smaller than middle element, search the items to the left of the middle element...this effectively halves the subarray that we are searching. Set right pointer to middle - 1, and repeat the loop.
+    3) However, if target is smaller than middle element, search the items to the left of the middle element...this effectively halves the subarray that we are searching. Set right pointer to middle - 1, and repeat the loop.
 
-		4) Similarly, if target is larger than middle element, search the items to the left of the middle element...this effectively halves the subarray that we are searching. Set left pointer to middle + 1, and repeat the loop.
+    4) Similarly, if target is larger than middle element, search the items to the left of the middle element...this effectively halves the subarray that we are searching. Set left pointer to middle + 1, and repeat the loop.
 
-	We should either find the target element in the array and have returned true, or if we exit the loop above, the target element was not in the array, and we should return false.
+  We should either find the target element in the array and have returned true, or if we exit the loop above, the target element was not in the array, and we should return false.
 
 Recursive Strategy:
 
