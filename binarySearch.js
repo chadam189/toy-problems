8f87ad402e9397b0e0d2d9e3ac06a97a374cb16a
Adam Miller <chadam189@gmail.com>
2018-02-13 19:56:46 -0800

    Create tests for binarySearch from CTCI

--- binarySearch.js
+++ binarySearch.js
@@ -1,4 +1,37 @@
+const binarySearchIterative = (nums, target) => {
+	// three variables to track:
+	  // left index of subarray (init to first index in array)
+	  // right index of subarray (init to last index in array)
+	  // middle index of subarray (init to (nums.length + 1) / 2)
+	// while -----
+	  // if element at middle index equals target
+	    // return true
+	  // else if we're searching a subarray of length 1, the target is not in this array
+	    // return false
+	  // else if target is larger than element at middle index
+	    // left = middle + 1
+	    // middle = ((left + right) / 2)
+	  // else if target is smaller than element at middle index
+	    // right = middle - 1
+	    // middle = ((left + right) / 2)
+	// return false
+};
 
+let nums = [1, 2, 4, 5, 7, 9, 12, 14, 16]
+let target = 2;
+let expected = true;
+let searchCount = 0;
+let expectedSearchCount = 2
+
+console.log(`Input of ${nums} and target of ${target} should return ${expected} with a total comparison count of ${expectedSearchCount}: ${binarySearchIterative(nums, target)} with ${searchCount} search(es)`);
+
+nums = [1, 2, 4, 5, 7, 9, 12, 14, 16]
+target = 3;
+expected = false;
+searchCount = 0;
+expectedSearchCount = 4
+
+console.log(`Input of ${nums} and target of ${target} should return ${expected} with a total comparison count of ${expectedSearchCount}: ${binarySearchIterative(nums, target)} with ${searchCount} search(es)`);
 
 
 /* 
