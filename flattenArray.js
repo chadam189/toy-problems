88988dac767c88fd0df0c8930f39dbcb64af6169
Adam Miller <chadam189@gmail.com>
2018-02-19 14:21:00 -0800

    Complete flattenArray from Canvas

--- flattenArray.js
+++ flattenArray.js
@@ -4,20 +4,21 @@ Build function to flatten an array
 
 */
 
-
-
 const flatten = (arr) => {
 	let results = [];
 
-	// for each element in input...
-	  // if element is array
-	    // recursively call flatten, and concat output to results
-    // else, push element onto results
+	arr.forEach((elem) => {
+		if (Array.isArray(elem)) {
+			results = results.concat(flatten(elem));
+		} else {
+			results.push(elem);
+		}
+	});
 	
-	// return results
+	return results;
 };
 
-let input = [1, 2, [3, 4], 5, [6, [7, 8], 9]], 10];
+let input = [1, 2, [3, 4], 5, [6, [7, 8], 9], 10];
 let result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log(`it should flatten nested arrays: ${JSON.stringify(flatten(input)) === JSON.stringify(result)}`);
@@ -46,8 +47,7 @@ Strategy:
 
 Create results array. Iterate through input array, adding elements to results array if element is a number/string or primitive data type. If element is an array, recursively call function, and concat the output to the results array.
 
-Big O: linear => O(n)
-
+Big O: linear => O(n), space => O(n)
 
 Input: let input = [1, 2, [3, 4], 5, [6, [7, 8], 9]], 10];
 
