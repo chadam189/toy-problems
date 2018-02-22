4f12b5b30e7af4ca34a3f3a40ef57baf31d52097
Adam Miller <chadam189@gmail.com>
2018-02-22 15:45:45 -0800

    Create skeleton for graphPractice

---
+++ graphPractice.js
@@ -0,0 +1,17 @@
+class Graph {
+  constructor (verticesCount) {
+  	this.verticesCount = verticesCount;
+  	this.adjacencyList = new Map();
+  }
+
+  // add vertex
+  // add edge
+  // remove edge
+  // remove vertex
+  // print graph
+
+  // dfs 
+  // bfs
+
+
+}
