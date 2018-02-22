77876b7833f72caa053016818c73176f346655a9
Adam Miller <chadam189@gmail.com>
2018-02-22 12:43:01 -0800

    Create setup for distributeCandies from LeetCode

--- distributeCandies.js
+++ distributeCandies.js
@@ -30,3 +30,54 @@ The number in given array is in range [-100,000, 100,000].
 
 
 */
+
+
+
+/* 
+
+SETUP 
+
+Strategy:
+
+	Iterate through candies, adding all candies to a map as keys with value initialized to 1. If a candy already exists, increment its value.
+
+	We now have a list of all candies with their values, and therefore we know how many unique candies there are. The most candies that one person can receive is (length of candies / 2). So, if we have as many or more unique candies than that max, we can return that. Otherwise, we'd give the sister the highest number of unique candies, so we'd return that number. 
+
+	Return minimum value between the length of map, and (candies length / 2).
+
+Big O = Linear for one loop over candies array. O(n).
+
+Input: let candies = [1,1,2,2,3,3];
+
+Output: let result = 3;
+
+
+const distributeCandies = (candies) => {
+	let candyTypes = new Map();
+
+	// for each candy
+	  // if the candy does not exist in map
+	     // add it to hash map as key with initial value of 1
+	  // else 
+	    // incrememnt its count by one
+
+	// return minimum value between (candies.length / 2) and candyTypes.size
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
