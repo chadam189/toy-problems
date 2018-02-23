fbce27dc0da03c9b9ca4d84faa3f5d1711fdba70
Adam Miller <chadam189@gmail.com>
2018-02-22 23:51:45 -0800

    Refactor overlapRectangles from InterviewCake

--- overlapRectangles.js
+++ overlapRectangles.js
@@ -1,39 +1,66 @@
+const findXOverlap = (a, b) => {
+
+  let [left, width] = [null, null];
+
+  if (a.left > b.left) {
+  	if (b.left + b.width > a.left) {
+  		left = a.left;
+  		result = Math.min(b.left + b.width - a.left, a.width);
+  	}
+
+  } else {
+    if (a.left + a.width > b.left) {
+			left = b.left;
+	    width = Math.min(a.left + a.width - b.left, b.width);
+    }
+  }
+  return [left, width];
+}
+
+const findYOverlap = (a, b) => {
+
+  let [bottom, height] = [null, null];
+
+  if (a.bottom > b.bottom) {
+  	if (b.bottom + b.height > a.bottom) {
+  		bottom = a.bottom;
+  		result = Math.min(b.bottom + b.height - a.bottom, a.height);
+  	}
+
+  } else {
+    if (a.bottom + a.height > b.bottom) {
+			bottom = b.bottom;
+	    height = Math.min(a.bottom + a.height - b.bottom, b.height);
+    }
+  }
+  return [bottom, height];
+}
+
 const overlapRectangles = (a, b) => {
 
-  const result = {
-  	left: null,
-  	bottom: null,
-  	width: null,
-  	height: null
-  }
-	// find if there is a valid left
+  let left = null;
+  let bottom = null;
+  let width = null;
+  let height = null;
 
-	if (a.width > b.left - a.left) {
-    result.left = b.left;
-    result.width = Math.min(a.left + a.width - b.left, b.width);
-	} else if (b.width > a.left - b.left) {
-    result.left = a.left;
-    result.width = Math.min(b.left + b.width - a.left, a.width);
-	} else {
-		return null;
-	}
+	// find horizontal overlap (if one exists)
 
-	// find if there is a valid bottom
+	[left, width] = findXOverlap(a, b);
 
-	if (a.height > b.bottom - a.bottom) {
-    result.bottom = b.bottom;
-    result.height = Math.min(a.bottom + a.height - b.bottom, b.height);
-	} else if (b.height > a.bottom - b.bottom) {
-    result.bottom = a.bottom;
-    result.height = Math.min(b.bottom + b.height - a.bottom, a.height);
-	} else {
-		return null;
-	}
+	[bottom, height] = findYOverlap(a, b);
 
-	// we have coordinates, return with calculated height/width
-  return result;
+	let result = {
+		left,
+		bottom,
+		width,
+		height
+	};
+
+	return result;
 
 };
+
+
 
 const a = {
 	left: 1,
@@ -50,8 +77,15 @@ const b = {
 };
 
 const c = {
-	left: 1,
+	left: 10,
 	bottom: 5,
+	width: 1,
+	height: 1
+};
+
+const d = {
+	left: 7,
+	bottom: 4,
 	width: 1,
 	height: 1
 };
@@ -63,15 +97,26 @@ const answerAB = {
 	height: 2
 };
 
-console.log(`expect A and B overlap to equal ${JSON.stringify(answerAB)}: ${JSON.stringify(overlapRectangles(a, b))}`);
+const nullAnswer = {
+	left: null,
+	bottom: null,
+	width: null,
+	height: null
+};
+
+// console.log(`X overlap of A and A: ${JSON.stringify(findXOverlap(a, a))}`);
+// console.log(`X overlap of A and B: ${JSON.stringify(findXOverlap(a, b))}`);
+// console.log(`X overlap of A and C: ${JSON.stringify(findXOverlap(a, c))}`);
+// console.log(`X overlap of A and D: ${JSON.stringify(findXOverlap(a, d))}`);
+
+// console.log(`Y overlap of A and A: ${JSON.stringify(findYOverlap(a, a))}`);
+// console.log(`Y overlap of A and B: ${JSON.stringify(findYOverlap(a, b))}`);
+// console.log(`Y overlap of A and C: ${JSON.stringify(findYOverlap(a, c))}`);
+// console.log(`Y overlap of A and D: ${JSON.stringify(findYOverlap(a, d))}`);
+
+console.log(`expect A and A overlap to equal ${JSON.stringify(a)} ${JSON.stringify(overlapRectangles(a, a)) === JSON.stringify(a)}`);
+console.log(`expect A and B overlap to equal ${JSON.stringify(answerAB)}: ${JSON.stringify(overlapRectangles(a, b)) === JSON.stringify(answerAB)}`);
 console.log(`expect A and C overlap to equal null: ${JSON.stringify(overlapRectangles(a, c))}`);
+console.log(`expect A and D overlap to equal null even though edges touch: ${JSON.stringify(overlapRectangles(a, d))}`);
 
-// 1 => 7
-// 1 => 4
-
-// 5 => 8
-// 2 => 8
-
-// 5 => 7
-// 2 => 4
 
