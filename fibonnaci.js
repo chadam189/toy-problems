222a5ee2c107558ff325c59f9b634d549e22e174
Adam Miller <chadam189@gmail.com>
2018-02-22 23:15:51 -0800

    Complete bottom-up solution for fibonnaci

--- fibonnaci.js
+++ fibonnaci.js
@@ -5,8 +5,7 @@ const fibRecursive = (n) => {
 	}
 
 	return fibRecursive(n - 1) + fibRecursive(n - 2);
-}
-
+};
 
 const fibMemo = (n) => {
 	let memo = {};
@@ -28,9 +27,27 @@ const fibMemo = (n) => {
   };
 
   return fibHelper(n);
-
-
 }
+
+const fibTailUp = (n) => {
+
+	if (n === 0 || n === 1) {
+		return 1;
+	}
+
+	let a = 0;
+	let b = 1;
+	let counter = 2;
+
+	while (counter <= n) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+    counter++;
+	}
+
+	return a + b;
+};
 
 
 
@@ -44,3 +61,7 @@ counter = 0;
 result = fibMemo(input);
 
 console.log(`Solving fib of ${input} with memoizing takes ${counter} calls: ${result}` );
+
+result = fibTailUp(input);
+
+console.log(`Solving fib of ${input} with 'tail up' takes ${1} call: ${result}` );
